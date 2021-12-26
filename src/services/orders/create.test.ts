73d4a88635a062f7f5a44844bb0f32ec87dbce3e
zyzhao <zyzhao@thoughtworks.com>
2021-12-26 20:05:30 +0800

    fix: test

--- src/services/orders/create.test.ts
+++ src/services/orders/create.test.ts
@@ -2,7 +2,7 @@ import { when } from 'jest-when';
 import { getRepository } from 'typeorm';
 
 import { SERVER_ERROR, SUCCESS, UNKNOWN_ERROR } from '../../consts';
-import { ORDER_CREATED } from '../../consts/ResponseMessages';
+import { ORDER_CREATED } from '../../consts';
 import { OrderEntity } from '../../typeorm/entities/orders/OrderEntity';
 import { OrderStatus } from '../../typeorm/entities/orders/types';
 import { ErrorType } from '../../types/CustomError';
@@ -40,14 +40,17 @@ describe('create order', () => {
   };
   const spyNext = jest.fn();
   const stubCreateOrder = jest.fn();
+  const stubSave = jest.fn();
   (getRepository as jest.Mock).mockReturnValue({
     create: stubCreateOrder,
+    save: stubSave,
   });
 
   it('should successfully create order', async () => {
     when(stubCreateOrder)
       .calledWith({ product_id: 'product_id', employee_id: 'employee_id' })
       .mockReturnValue(repositoryCreationEntity);
+    when(stubSave).calledWith(repositoryCreationEntity).mockReturnValue(repositoryCreationEntity);
 
     await create(request, fakeResponse, spyNext);
 
